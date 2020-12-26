e46c324d740daa80c952e3586e86f8d94dbc58fc
Constantin Zorile <czorile@gmail.com>
2020-12-27 00:12:09 +0200

    add -M argument to the compiler

--- hash-table-c/src/compile.bat
+++ hash-table-c/src/compile.bat
@@ -1,5 +1,5 @@
 
 :: C99
 @echo off
-gcc -Wall -Wmissing-prototypes -Wstrict-prototypes -Wextra -D _GNU_SOURCE -O2 -std=c99 -ggdb3 *.c -o ht_table_app
+gcc -M -Wall -Wmissing-prototypes -Wstrict-prototypes -Wextra -D _GNU_SOURCE -O2 -std=c99 -ggdb3 *.c -o ht_table_app
 
