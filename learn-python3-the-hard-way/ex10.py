3d9a2ba503655d0f3cd832bde75c5a4d99cb1a43
Decameron <czorile@gmail.com>
2021-01-02 00:38:05 +0200

    passing argv to python

--- learn-python3-the-hard-way/ex10.py
+++ learn-python3-the-hard-way/ex10.py
@@ -1,12 +1,38 @@
 
 # ex.10
-print('\n##### ex. 10')
+print('\n*************** ex. 10')
+# print("age?", end=' ')
+# age = input()
+# print("height?", end=' ')
+# height = input()
+# print("weight?", end=' ')
+# weight = input()
+# print(f"So, age is {age}, height is {height} and weight is {weight}")
 
-print("age?", end=' ')
-age = input()
-print("height?", end=' ')
-height = input()
-print("weight?", end=' ')
-weight = input()
 
-print(f"So, age is {age}, height is {height} and weight is {weight}")
+# ex.12
+print('\n*************** ex. 12')
+# age = input('age? ')
+# height = input('height? ')
+# weight = input('weight? ')
+# print(f"So, age is {age}, height is {height} and weight is {weight}")
+
+# # LearnXinYminutes: Python
+# print('-5 // 2 ', -5 // 2)
+# print('-5 // 3.0 ', -5 // 3.0)
+# print('-7 % 3 ', -7 % 3)
+# print('2**3 ', 2**3)
+# print('not False: ', not False)
+# print('bool(0) ', bool(0))
+# print('bool(-6) ', bool(-6))
+# print('0 and 3 ', 0 and 3)
+
+
+# ex.13
+print('\n*************** ex. 13')
+from sys import argv
+script, first, second, third = argv
+print('the script is called: ', script)
+print('your first variable is: ', first)
+print('your second variable is: ', second)
+print('your third variable is: ', third)
