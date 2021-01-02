2a0f3a71c8e8bf17b74aa3b3eeceaae7d32c5514
Decameron <czorile@gmail.com>
2021-01-03 00:12:28 +0200

    add fetchIssues and (partially) saveIssue()

--- js-issue-tracker/main.js
+++ js-issue-tracker/main.js
@@ -1,5 +1,43 @@
-function testFunction() {
-  console.log("JS working");
+/*
+ * This issue data is saved in broswer's Local Storage (LS).
+ * This function retrieves the issues from LS.
+ */
+function fetchIssues() {
+  let issues = JSON.parse(localStorage.getItem('issues'));
+  let issuesList = document.getElementById('issuesList');
+
+  issuesList.innerHTML = '';
+
+  for (let i = 0; i < issues.length; i++) {
+    let id  = issues[i].id;
+    let desc = issues[i].description;
+    let severity = issues[i].severity;
+    let assignedTo = issues[i].assignedTo;
+    let status = issues[i].status;
+
+    issuesList.innerHTML =
+      issuesList.innerHTML + '<div class="well">' + 
+      '<h6>Issue ID: ' + id + '</h6>' + 
+      '<p><span class="label label-info">' + status + '</status></p>' +
+      '<h3>' + desc + '</h3>' + 
+      '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
+      '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+      '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
+      '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+      '</div>';
+  }
 }
 
-testFunction();
+
+/* Save issue data to LS after form submit. */
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  let issueId = chance.guid();
+  let issueDesc = document.getElementById('issueDescInput').value;
+  let issueSeverity = document.getElementById('issueSeverityInput').value;
+  let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  let issueStatus = 'Open';
+
+  // more to follow
+}
